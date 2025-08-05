c494a0d9dcc30b09699b649a3bf7f4e37fc9b1e4
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-08-04 20:06:27 -0500

    first commit

---
+++ ProjectManager.WebApi/Controllers/ProjectController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager.WebApi.Models.Requests.Project;
+
+namespace ProjectManager.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateProjectRequest request)
+        {
+            try
+            {
+                return Ok(request);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        // getById/ce5c4d95-dd15-4e8b-bb3a-7bf19c46f40a
+        [HttpGet("/getById/{projectId:guid}/{number:int}")]
+        public IActionResult GetById(Guid projectId, int number)
+        {
+            try
+            {
+                return Ok(new
+                {
+                    ProjectId = projectId,
+                    Number = number
+                });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        // Agregar metodo para actualizar un proyecto
+
+        // Agregar metodo para eliminar un proyecto
+    }
+}
---
+++ ProjectManager.WebApi/Models/Requests/Project/CreateProjectRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManager.WebApi.Models.Requests.Project
+{
+    public class CreateProjectRequest
+    {
+        [MaxLength(50, ErrorMessage = "El nombre del proyecto es muy largo")]
+        [MinLength(3)]
+        public required string Name { get; set; }
+        [MaxLength(500, ErrorMessage = "La descripción del proyecto es muy larga")]
+        [MinLength(10)]
+        public required string Description { get; set; }
+        public int StatusId { get; set; } = 1;
+        [MaxLength(255, ErrorMessage = "El banner del proyecto es muy largo")]
+        [MinLength(15)]
+        public required string Banner { get; set; }
+    }
+}
